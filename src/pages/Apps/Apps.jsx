14f799ee30e5b3f322cf446949194f6e35fe3809
mdriyazakondo <mdriyazakondo265@gmail.com>
2025-10-09 17:15:39 +0600

    apps page

--- src/pages/Apps/Apps.jsx
+++ src/pages/Apps/Apps.jsx
@@ -51,7 +51,7 @@ const Apps = () => {
       ) : (
         <div className="max-w-[1500px] mx-5 md:mx-auto">
           <div>
-            <h2 className="text-3xl md:text-4xl font-bold">
+            <h2 className="text-3xl md:text-5xl font-bold mb-2 text-center">
               Our All Applications
             </h2>
             <p className="text-gray-600 text-sm md:text-xl font-medium text-center">
